902bcd2985ad3a627bb8d23fa4bf560fe743dccc
James Lane Sheaf-Morrison <jlanesm@gmail.com>
2019-06-06 14:30:49 +1200

    The two URLs have been created

--- app.py
+++ app.py
@@ -7,19 +7,12 @@ app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+@app.route('/isPrime/<int:number>')
+def isPrime(number):
+    return '%d is prime.\n' % number
 
+@app.route('/primesStored')
+def primesStored():
+    count = 2
+    return '{} ~number list.\n'.format(count)
 
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
