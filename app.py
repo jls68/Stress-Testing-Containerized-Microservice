a284affb3b4f8079678e9c5631e8c88388c85581
James Lane Sheaf-Morrison <jlanesm@gmail.com>
2019-06-06 17:25:31 +1200

    primesStored displayed stored primes

--- app.py
+++ app.py
@@ -4,28 +4,38 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+storage = redis.Redis(host='redis', port=6379)
 
 
 @app.route('/isPrime/<int:number>')
 def isPrime(number):
     if (check_if_prime(number)):
+        storage.lrem('primeList', 0, number)
+        # Add prime number to the head of the list the redis object
+        storage.lpush('primeList', number)
         return '%d is prime.\n' % number
     return '%d is not prime.\n' % number
 
+# Return true if x is prime or false if not
 def check_if_prime(x):
     if 2 == x:
         return True
     if 0 == x % 2 or 1 >= x:
         return False
-    sqRoot = int(math.sqrt(x)) + 1
-    for factor in range(3, sqRoot, 2):
-        if 0 == x % factor:
+    xSqRoot = int(math.sqrt(x)) + 1
+    # Check for possible factors other than 2 and 1
+    for possiblefactor in range(3, xSqRoot, 2):
+        if 0 == x % possiblefactor:
             return False
     return True
 
 @app.route('/primesStored')
 def primesStored():
-    count = 2
-    return '{} ~number list.\n'.format(count)
+    # Print list of prime numbers in redis object
+    primes = ""
+    list = storage.lrange('primeList', 0, -1)
+    for num in list:
+            primes += str(int(num)) + " "
+    return primes
+  
 
