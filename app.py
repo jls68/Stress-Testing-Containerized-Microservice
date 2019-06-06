89b13ae3162587b09b70713339bfcca2c6ffad28
James Lane Sheaf-Morrison <jlanesm@gmail.com>
2019-06-06 14:39:26 +1200

    isPrime will check if number is prime

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import time
-
+import math
 import redis
 from flask import Flask
 
@@ -9,7 +9,20 @@ cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/isPrime/<int:number>')
 def isPrime(number):
-    return '%d is prime.\n' % number
+    if (check_if_prime(number)):
+        return '%d is prime.\n' % number
+    return '%d is not prime.\n' % number
+
+def check_if_prime(x):
+    if 2 == x:
+        return True
+    if 0 == x % 2 or 1 >= x:
+        return False
+    sqRoot = int(math.sqrt(x)) + 1
+    for factor in range(3, sqRoot, 2):
+        if 0 == x % factor:
+            return False
+    return True
 
 @app.route('/primesStored')
 def primesStored():
